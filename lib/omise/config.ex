0938c847f817179c00ca892a85a9ca97cbc755a7
teerawat1992 <teerawat.lamanchart@gmail.com>
2016-09-11 00:00:42 +0700

    Fix Elixir 1.4 warnings

--- lib/omise/config.ex
+++ lib/omise/config.ex
@@ -1,14 +1,12 @@
 defmodule Omise.Config do
   @moduledoc false
 
+  @permitted_params [:public_key, :secret_key, :api_version]
+
   @doc false
   def configure(params) do
     params
-    |> Enum.filter(fn {k, _} -> k in permitted_params end)
+    |> Enum.filter(fn {k, _} -> k in @permitted_params end)
     |> Enum.map(fn {k, v} -> Application.put_env(:omise, k, v) end)
-  end
-
-  defp permitted_params do
-    [:public_key, :secret_key, :api_version]
   end
 end
