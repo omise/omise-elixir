8ddb56ac6121eef572d199bded8fa069035d8eca
Teerawat Lamanchart <teerawat.lamanchart@gmail.com>
2016-09-22 01:19:05 +0700

    Add document API (#1)
    * Add document api
    
    * Add document tests
    
    * Update .travis.yml
    
    * Implement __using__ macro in Document module
    
    - Use this macro with Dispute module.
    - Add tests.
    
    * Rename module attr in Document __using__ macro
    
    - Rename @doc_module to @document.
    - Create resource_path/1 private function.

--- test/omise/dispute_test.exs
+++ test/omise/dispute_test.exs
@@ -2,11 +2,8 @@ defmodule Omise.DisputeTest do
   use ExUnit.Case
   import TestHelper
 
-  setup_all do
-    dispute_id = "dspt_test_5089off452g5m5te7xs"
-
-    {:ok, dispute_id: dispute_id}
-  end
+  @dispute_id "dspt_test_5089off452g5m5te7xs"
+  @document_id "docu_test_55alihcdq6r2upo9slb"
 
   test "list all disputes" do
     with_mock_request "disputes-get", fn ->
@@ -28,9 +25,9 @@ defmodule Omise.DisputeTest do
     end
   end
 
-  test "retrieve a dispute", %{dispute_id: dispute_id} do
-    with_mock_request "disputes/#{dispute_id}-get", fn ->
-      {:ok, dispute} = Omise.Dispute.retrieve(dispute_id)
+  test "retrieve a dispute" do
+    with_mock_request "disputes/#{@dispute_id}-get", fn ->
+      {:ok, dispute} = Omise.Dispute.retrieve(@dispute_id)
 
       assert %Omise.Dispute{} = dispute
       assert dispute.object == "dispute"
@@ -46,14 +43,68 @@ defmodule Omise.DisputeTest do
     end
   end
 
-  test "update a dispute", %{dispute_id: dispute_id} do
-    with_mock_request "disputes/#{dispute_id}-patch", fn ->
+  test "update a dispute" do
+    with_mock_request "disputes/#{@dispute_id}-patch", fn ->
       message = "Take me to your best friend house"
-      {:ok, dispute} = Omise.Dispute.update(dispute_id, message: message)
+      {:ok, dispute} = Omise.Dispute.update(@dispute_id, message: message)
 
       assert %Omise.Dispute{} = dispute
       assert dispute.object == "dispute"
       assert dispute.message == message
+    end
+  end
+
+  test "list all documents" do
+    with_mock_request "disputes/#{@dispute_id}/documents-get", fn ->
+      {:ok, list} = Omise.Dispute.list_documents(@dispute_id)
+
+      assert %Omise.List{data: documents} = list
+      assert list.object == "list"
+      assert list.from
+      assert list.to
+      assert list.offset
+      assert list.limit
+      assert list.total
+      assert is_list(list.data)
+
+      Enum.each documents, fn(document) ->
+        assert %Omise.Document{} = document
+        assert document.object == "document"
+      end
+    end
+  end
+
+  test "retrieve a document" do
+    with_mock_request "disputes/#{@dispute_id}/documents/#{@document_id}-get", fn ->
+      {:ok, document} = Omise.Dispute.retrieve_document(@dispute_id, @document_id)
+
+      assert %Omise.Document{} = document
+      assert document.object == "document"
+      assert document.id
+      refute document.livemode
+      assert document.filename
+    end
+  end
+
+  test "upload a document" do
+    with_mock_request "disputes/#{@dispute_id}/documents-post", fn ->
+      {:ok, document} = Omise.Dispute.upload_document(@dispute_id, file: "screenshot.png")
+
+      assert %Omise.Document{} = document
+      assert document.object == "document"
+      assert document.id
+      refute document.livemode
+      assert document.filename
+    end
+  end
+
+  test "destroy a document" do
+    with_mock_request "disputes/#{@dispute_id}/documents-delete", fn ->
+      {:ok, document} = Omise.Dispute.destroy_document(@dispute_id, @document_id)
+
+      assert %Omise.Document{} = document
+      assert document.object == "document"
+      assert document.deleted
     end
   end
 end
