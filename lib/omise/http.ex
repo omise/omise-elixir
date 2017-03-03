884898eb6acb524102bf9da339c374c6337cb04e
teerawat1992 <teerawat.lamanchart@gmail.com>
2017-03-04 03:48:22 +0700

    Provide http_options config

--- lib/omise/http.ex
+++ lib/omise/http.ex
@@ -4,8 +4,8 @@ defmodule Omise.HTTP do
   alias Omise.{Utils, Version}
 
   @vault_endpoints ~w(tokens)
-  @base_vault_uri  Application.get_env(:omise, :base_vault_uri, "https://vault.omise.co/")
-  @base_api_uri    Application.get_env(:omise, :base_api_uri, "https://api.omise.co/")
+  @base_vault_uri  "https://vault.omise.co/"
+  @base_api_uri    "https://api.omise.co/"
 
   @spec get(String.t, Keyword.t, Keyword.t) :: {:ok, struct} | {:error, struct}
   def get(endpoint, query_params \\ [], opts \\ []) do
@@ -37,9 +37,10 @@ defmodule Omise.HTTP do
     body_params  = Keyword.get(request_params, :body_params, [])
 
     {url, auth_params} = url_with_auth_params(endpoint, key)
-    request_body       = Utils.encode_to_json(body_params)
-    request_headers    = req_headers(api_version)
-    request_options    = [params: query_params] ++ auth_params
+
+    request_body    = Utils.encode_to_json(body_params)
+    request_headers = req_headers(api_version)
+    request_options = [params: query_params] ++ auth_params ++ http_options()
 
     method
     |> HTTPoison.request!(url, request_body, request_headers, request_options)
@@ -56,16 +57,15 @@ defmodule Omise.HTTP do
     end
   end
 
+  defp url_with_auth_params(endpoint, key) when endpoint in @vault_endpoints do
+    key      = key || Application.get_env(:omise, :public_key)
+    base_uri = Application.get_env(:omise, :base_vault_uri, @base_vault_uri)
+    {base_uri <> endpoint, auth_params(key)}
+  end
   defp url_with_auth_params(endpoint, key) do
-    if endpoint in @vault_endpoints do
-      key = key || Application.get_env(:omise, :public_key)
-
-      {@base_vault_uri <> endpoint, auth_params(key)}
-    else
-      key = key || Application.get_env(:omise, :secret_key)
-
-      {@base_api_uri <> endpoint, auth_params(key)}
-    end
+    key      = key || Application.get_env(:omise, :secret_key)
+    base_uri = Application.get_env(:omise, :base_api_uri, @base_api_uri)
+    {base_uri <> endpoint, auth_params(key)}
   end
 
   defp auth_params(key) do
@@ -84,7 +84,14 @@ defmodule Omise.HTTP do
     %{"User-Agent" => user_agent(), "Content-type" => "application/json"}
   end
 
-  def user_agent do
+  defp user_agent do
     "OmiseElixir/#{Version.project_version} Elixir/#{Version.elixir_version}"
+  end
+
+  def http_options do
+    Keyword.merge(
+      [recv_timeout: 8_000],
+      Application.get_env(:omise, :http_options, [])
+    )
   end
 end
