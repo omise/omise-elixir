0206237fd86fc578e1d9116620054985599eff76
teerawat1992 <teerawat.lamanchart@gmail.com>
2017-05-30 18:29:38 +0700

    Refactor encode_to_json/1 function
    Make it support both list and keyword list

--- lib/omise/utils.ex
+++ lib/omise/utils.ex
@@ -4,20 +4,18 @@ defmodule Omise.Utils do
   @spec encode_to_json(Keyword.t) :: iodata | no_return
   def encode_to_json(body_params) when is_list(body_params) do
     body_params
-    |> normalize_req_body_params()
+    |> normalize_params()
     |> Poison.encode!
   end
 
-  defp normalize_req_body_params(params) when is_list(params) do
+  defp normalize_params(params) when is_list(params) do
     params
     |> Enum.map(&transform_value/1)
     |> Enum.into(%{})
   end
 
-  defp transform_value({key, value}) when is_list(value) do
-    {key, Enum.into(normalize_req_body_params(value), %{})}
-  end
-  defp transform_value(kv) do
-    kv
-  end
+  defp transform_value({key, [head | _] = value}) when is_tuple(head),
+    do: {key, Enum.into(normalize_params(value), %{})}
+  defp transform_value(kv),
+    do: kv
 end
