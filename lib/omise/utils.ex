a947702ef471636b40fb5c30cb33a81c4cd16f9a
Teerawat <teerawat.lamanchart@gmail.com>
2017-06-07 19:39:39 +0700

    Fix an issue when making a requset to Search api (#16)
    - Create normalize_search_params/1 utils function.
    - Apply this function to execute/3 function of Search module.

--- lib/omise/utils.ex
+++ lib/omise/utils.ex
@@ -18,4 +18,17 @@ defmodule Omise.Utils do
     do: {key, Enum.into(normalize_params(value), %{})}
   defp transform_value(kv),
     do: kv
+
+  def normalize_search_params(params) do
+    filters_params = params[:filters] || []
+    params
+    |> Keyword.take([:query])
+    |> Keyword.merge(normalize_filters_params(filters_params))
+  end
+
+  defp normalize_filters_params(filters_params) do
+    Enum.flat_map filters_params, fn({key, value}) ->
+      ["filters[#{key}]": value]
+    end
+  end
 end
--- test/omise/utils_test.exs
+++ test/omise/utils_test.exs
@@ -40,4 +40,42 @@ defmodule Omise.UtilsTest do
       assert Utils.encode_to_json(input) == expected_output
     end
   end
+
+  test "normalize search query params" do
+    test_cases = [
+      %{
+        input:  [query: "foo"],
+        output: [query: "foo"]
+      },
+
+      %{
+        input:  [query: "foo", foo: "bar", filter: [hello: "world"]],
+        output: [query: "foo"]
+      },
+
+      %{
+        input:  [],
+        output: []
+      },
+
+      %{
+        input:  [query: "foo", filters: []],
+        output: [query: "foo"]
+      },
+
+      %{
+        input:  [query: "foo", filters: [foo: "bar"]],
+        output: [query: "foo", "filters[foo]": "bar"]
+      },
+
+      %{
+        input:  [filters: [foo: "bar"]],
+        output: ["filters[foo]": "bar"]
+      },
+    ]
+
+    Enum.each test_cases, fn(%{input: input, output: expected_output}) ->
+      assert Utils.normalize_search_params(input) == expected_output
+    end
+  end
 end
