7c0f9857327f8336a617b9b7d8de80fd66354496
teerawat1992 <teerawat.lamanchart@gmail.com>
2017-11-04 13:28:56 +0700

    Refactor BankAccount

--- lib/omise/bank_account.ex
+++ lib/omise/bank_account.ex
@@ -5,25 +5,25 @@ defmodule Omise.BankAccount do
   <https://www.omise.co/bank-account-api>
   """
 
-  defstruct [
-    object:       "bank_account",
-    last_digits:  nil,
-    name:         nil,
-    created:      nil,
-    brand:        nil,
-    bank_code:    nil,
-    branch_code:  nil,
-    account_type: nil,
-  ]
+  defstruct object: "bank_account",
+            brand: nil,
+            bank_code: nil,
+            branch_code: nil,
+            account_type: nil,
+            last_digits: nil,
+            name: nil,
+            created: nil,
+            deleted: false
 
   @type t :: %__MODULE__{
-    object:       String.t,
-    last_digits:  String.t,
-    name:         String.t,
-    created:      String.t,
-    brand:        String.t,
-    bank_code:    String.t,
-    branch_code:  String.t,
-    account_type: String.t
-  }
+          object: String.t(),
+          brand: String.t(),
+          bank_code: String.t(),
+          branch_code: String.t(),
+          account_type: String.t(),
+          last_digits: String.t(),
+          name: String.t(),
+          created: String.t(),
+          deleted: boolean
+        }
 end
