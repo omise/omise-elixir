aebf7d97c170ddcdc6412e0c5b988f5b3fe68b83
teerawat1992 <teerawat.lamanchart@gmail.com>
2017-11-04 14:22:36 +0700

    Refactor Utils

--- lib/omise/utils.ex
+++ lib/omise/utils.ex
@@ -1,11 +1,18 @@
 defmodule Omise.Utils do
   @moduledoc false
 
-  @spec encode_to_json(Keyword.t) :: iodata | no_return
   def encode_to_json(body_params) when is_list(body_params) do
     body_params
     |> normalize_params()
-    |> Poison.encode!
+    |> Poison.encode!()
+  end
+
+  def normalize_search_params(params) do
+    filters_params = params[:filters] || []
+
+    params
+    |> Keyword.take([:query])
+    |> Keyword.merge(normalize_filters_params(filters_params))
   end
 
   defp normalize_params(params) when is_list(params) do
@@ -16,19 +23,12 @@ defmodule Omise.Utils do
 
   defp transform_value({key, [head | _] = value}) when is_tuple(head),
     do: {key, Enum.into(normalize_params(value), %{})}
-  defp transform_value(kv),
-    do: kv
 
-  def normalize_search_params(params) do
-    filters_params = params[:filters] || []
-    params
-    |> Keyword.take([:query])
-    |> Keyword.merge(normalize_filters_params(filters_params))
-  end
+  defp transform_value(kv), do: kv
 
   defp normalize_filters_params(filters_params) do
-    Enum.flat_map filters_params, fn({key, value}) ->
+    Enum.flat_map(filters_params, fn {key, value} ->
       ["filters[#{key}]": value]
-    end
+    end)
   end
 end
