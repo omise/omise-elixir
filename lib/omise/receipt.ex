d090a42420d48b7121282144cdd652884d045618
teerawat1992 <teerawat.lamanchart@gmail.com>
2017-11-04 14:00:11 +0700

    Refactor Receipt

--- lib/omise/receipt.ex
+++ lib/omise/receipt.ex
@@ -5,59 +5,55 @@ defmodule Omise.Receipt do
   <https://www.omise.co/receipt-api>
   """
 
-  import Omise.HTTP
+  use Omise.HTTPClient, endpoint: "receipts"
 
-  defstruct [
-    object:                  "receipt",
-    id:                      nil,
-    number:                  nil,
-    location:                nil,
-    date:                    nil,
-    customer_name:           nil,
-    customer_address:        nil,
-    customer_tax_id:         nil,
-    customer_email:          nil,
-    customer_statement_name: nil,
-    company_name:            nil,
-    company_address:         nil,
-    company_tax_id:          nil,
-    charge_fee:              nil,
-    voided_fee:              nil,
-    transfer_fee:            nil,
-    subtotal:                nil,
-    vat:                     nil,
-    wht:                     nil,
-    total:                   nil,
-    credit_note:             false,
-    currency:                nil
-  ]
+  defstruct object: "receipt",
+            id: nil,
+            number: nil,
+            location: nil,
+            date: nil,
+            customer_name: nil,
+            customer_address: nil,
+            customer_tax_id: nil,
+            customer_email: nil,
+            customer_statement_name: nil,
+            company_name: nil,
+            company_address: nil,
+            company_tax_id: nil,
+            charge_fee: nil,
+            voided_fee: nil,
+            transfer_fee: nil,
+            subtotal: nil,
+            vat: nil,
+            wht: nil,
+            total: nil,
+            credit_note: false,
+            currency: nil
 
   @type t :: %__MODULE__{
-    object:                  String.t,
-    id:                      String.t,
-    number:                  String.t,
-    location:                String.t,
-    date:                    String.t,
-    customer_name:           String.t,
-    customer_address:        String.t,
-    customer_tax_id:         String.t,
-    customer_email:          String.t,
-    customer_statement_name: String.t,
-    company_name:            String.t,
-    company_address:         String.t,
-    company_tax_id:          String.t,
-    charge_fee:              integer,
-    voided_fee:              integer,
-    transfer_fee:            integer,
-    subtotal:                integer,
-    vat:                     integer,
-    wht:                     integer,
-    total:                   integer,
-    credit_note:             boolean,
-    currency:                String.t
-  }
-
-  @endpoint "receipts"
+          object: String.t(),
+          id: String.t(),
+          number: String.t(),
+          location: String.t(),
+          date: String.t(),
+          customer_name: String.t(),
+          customer_address: String.t(),
+          customer_tax_id: String.t(),
+          customer_email: String.t(),
+          customer_statement_name: String.t(),
+          company_name: String.t(),
+          company_address: String.t(),
+          company_tax_id: String.t(),
+          charge_fee: integer,
+          voided_fee: integer,
+          transfer_fee: integer,
+          subtotal: integer,
+          vat: integer,
+          wht: integer,
+          total: integer,
+          credit_note: boolean,
+          currency: String.t()
+        }
 
   @doc ~S"""
   List all receipts.
@@ -77,7 +73,7 @@ defmodule Omise.Receipt do
       Omise.Receipt.list(limit: 5)
 
   """
-  @spec list(Keyword.t, Keyword.t) :: {:ok, Omise.List.t} | {:error, Omise.Error.t}
+  @spec list(Keyword.t(), Keyword.t()) :: {:ok, Omise.List.t()} | {:error, Omise.Error.t()}
   def list(params \\ [], opts \\ []) do
     opts = Keyword.merge(opts, as: %Omise.List{data: [%__MODULE__{}]})
     get(@endpoint, params, opts)
@@ -93,7 +89,7 @@ defmodule Omise.Receipt do
       Omise.Receipt.retrieve("rcpt_test_4z6p7e0m4k40txecj5o")
 
   """
-  @spec retrieve(String.t, Keyword.t) :: {:ok, t} | {:error, Omise.Error.t}
+  @spec retrieve(String.t(), Keyword.t()) :: {:ok, t} | {:error, Omise.Error.t()}
   def retrieve(id, opts \\ []) do
     opts = Keyword.merge(opts, as: %__MODULE__{})
     get("#{@endpoint}/#{id}", [], opts)
