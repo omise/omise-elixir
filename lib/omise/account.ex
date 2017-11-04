c7e7e2c9f394d3256aeb43c10814121ddcb47815
teerawat1992 <teerawat.lamanchart@gmail.com>
2017-11-04 13:24:43 +0700

    Refactor Account

--- lib/omise/account.ex
+++ lib/omise/account.ex
@@ -5,27 +5,27 @@ defmodule Omise.Account do
   <https://www.omise.co/account-api>
   """
 
-  import Omise.HTTP
+  use Omise.HTTPClient, endpoint: "account"
 
-  defstruct [
-    object:   "account",
-    id:       nil,
-    location: nil,
-    email:    nil,
-    currency: nil,
-    created:  nil
-  ]
+  defstruct object: "account",
+            id: nil,
+            livemode: nil,
+            location: nil,
+            email: nil,
+            currency: nil,
+            supported_currencies: nil,
+            created: nil
 
   @type t :: %__MODULE__{
-    object:   String.t,
-    id:       String.t,
-    location: String.t,
-    email:    String.t,
-    currency: String.t,
-    created:  String.t
-  }
-
-  @endpoint "account"
+          object: String.t(),
+          id: String.t(),
+          livemode: boolean,
+          location: String.t(),
+          email: String.t(),
+          currency: String.t(),
+          supported_currencies: list,
+          created: String.t()
+        }
 
   @doc ~S"""
   Retrieve the account.
@@ -39,7 +39,7 @@ defmodule Omise.Account do
       Omise.Account.retrieve(key: "skey_56ia27lqtvg32u7iww8")
 
   """
-  @spec retrieve(Keyword.t) :: {:ok, t} | {:error, Omise.Error.t}
+  @spec retrieve(Keyword.t()) :: {:ok, t} | {:error, Omise.Error.t()}
   def retrieve(opts \\ []) do
     opts = Keyword.merge(opts, as: %__MODULE__{})
     get(@endpoint, [], opts)
